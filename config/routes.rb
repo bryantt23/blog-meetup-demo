afe42082c908b1305f3934afb84c2961daf09dad
bryantt23 <bryantt23@gmail.com>
2015-04-08 01:09:50 -0700

    started index display

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
-  root 'article#index'
+  root 'articles#index'
 
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
